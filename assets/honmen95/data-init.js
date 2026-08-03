a9ed8769f8aa5d5ce02306592957a56953727362
ngocbabby <meliingshop1809@gmail.com>
2026-08-04 08:20:02 +0900

    Load cropped PDF images before initializing Honmen data

--- assets/honmen95/data-init.js
+++ assets/honmen95/data-init.js
@@ -1,1 +1,5 @@
+window.HONMEN_IMAGES=window.HONMEN_IMAGES||{};
+window.HONMEN_IMAGES['theory-signal-demo']='assets/honmen95/theory-signal-demo.jpg';
+window.HONMEN_IMAGES['q34-source']='assets/honmen95/q34-source-demo.jpg';
+window.HONMEN_IMAGES['scenario-91']='assets/honmen95/question91-scene-demo.jpg';
 window.HONMEN_DATA={regular:window.HONMEN_REGULAR||[],scenarios:window.HONMEN_SCENARIOS||[],images:window.HONMEN_IMAGES||{}};
