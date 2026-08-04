8d0221c589fefc318f914774464db9b31550e71f
ngocbabby <meliingshop1809@gmail.com>
2026-08-04 11:57:45 +0900

    Restore stable Honmen app loading

--- assets/honmen95/data-init.js
+++ assets/honmen95/data-init.js
@@ -1,12 +1,7 @@
 window.HONMEN_IMAGES=window.HONMEN_IMAGES||{};
-// Không dùng các ảnh JPEG thử bị nén mờ nữa.
-window.HONMEN_DATA={regular:window.HONMEN_REGULAR||[],scenarios:window.HONMEN_SCENARIOS||[],images:window.HONMEN_IMAGES};
-[
-  'assets/honmen95/images-s91-hq.js',
-  'assets/honmen95/image-viewer.js'
-].forEach(src=>{
-  const s=document.createElement('script');
-  s.src=src+'?v=20260804-2';
-  s.async=false;
-  document.head.appendChild(s);
-});
+window.HONMEN_IMAGES['scenario-91']='assets/honmen95/question91-scene-demo.jpg';
+window.HONMEN_DATA={
+  regular:window.HONMEN_REGULAR||[],
+  scenarios:window.HONMEN_SCENARIOS||[],
+  images:window.HONMEN_IMAGES
+};
